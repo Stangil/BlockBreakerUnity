a296e3ee441f5f3b224333905663e9f4e1333bfe
Stangil <stangil44@gmail.com>
2020-04-27 14:57:33 -0400

    Set up move paddle and launch ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float launchBallX = 2f;
+    [SerializeField] float launchBallY = 15f;
+
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseCLick();
+        }
+    }
+
+    private void LaunchOnMouseCLick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(launchBallX, launchBallY);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float xMin = 1f;
+    [SerializeField] float xMax = 15f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMousePosInUnits = Mathf.Clamp((Input.mousePosition.x / Screen.width * screenWidthInUnits),xMin, xMax);
+        Vector2 paddlePosition = new Vector2(xMousePosInUnits, transform.position.y);
+        transform.position = paddlePosition;
+    }
+}
